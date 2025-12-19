002d4ca36a133e8e5f08d7a3b1a753d4fdad55bc
Turgut <turcuteforwebsites@proton.me>
2025-12-19 15:19:04 +0300

    feat: odev8 tamamlandi - kullanici 0 girene kadar devam eden sorgu (while ile)

---
+++ js/odev8.js
@@ -0,0 +1,8 @@
+
+let number = Number(prompt(`Please enter number: (Type 0 to exit)`))
+
+while (number !== 0) {
+  console.log(`The number entered is ${number}`);
+  number = Number(prompt(`Please enter number: (Type 0 to exit)`))
+} 
+
