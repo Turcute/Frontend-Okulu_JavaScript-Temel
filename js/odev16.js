081d02cc3602a2364e2bdbde06462cc3147741d1
linux-dev <turcuteforwebsites@proton.me>
2025-12-28 13:24:28 +0300

    feat: odev16 tamamlandi - Birden fazla kullanici girdisini diziye ekleme (push)

---
+++ js/odev16.js
@@ -0,0 +1,11 @@
+
+let task = [];
+
+let userTask = Number(prompt("How many tasks would you like to add?"));
+
+for (let i = 0; i < userTask; i++) {
+  let input = prompt("Enter task name");
+  task.push(input);
+}
+
+console.log(`Your tasks: ${task}`);
