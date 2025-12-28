9be8476e59360dd2efdc34cab3ce076ad7560cae
linux-dev <turcuteforwebsites@proton.me>
2025-12-28 13:12:47 +0300

    feat: odev15 tamamlandi - Kullanicidan alinan elemani diziye ekleme (push)

---
+++ js/odev15.js
@@ -0,0 +1,10 @@
+// Kullanıcıdan Alınan Elemanı Diziye Ekleme (push)
+
+
+let cities = ["İstanbul", "Ankara", "İzmir"];
+
+let userInput = prompt(`Enter a city name`)
+
+cities.push(userInput)
+
+console.log(`${cities}`)
