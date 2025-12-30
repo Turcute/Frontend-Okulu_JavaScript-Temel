d4a1d0e9978d02b330c55921dfd24c660d51adc1
Turgut <turcuteforwebsites@proton.me>
2025-12-30 10:25:09 +0300

    feat: odev19 tamamlandi - Listeden ilk elemani kaldirma (shift)

---
+++ js/odev19.js
@@ -0,0 +1,8 @@
+// Listeden İlk Elemanı Kaldırma (shift)
+
+let persons = ["Ahmet", "Ayşe", "Mehmet"];
+console.log (`All Persons: ${persons}`)
+
+let deletedPerson = persons.shift();
+
+console.log(`Deleted Person: ${deletedPerson}`)
