90bc3d9d8eecf152b756cbe2d68cf4698dbde03a
Turgut <turcuteforwebsites@proton.me>
2026-01-23 09:41:20 +0300

    feat: odev32 tamamlandi - Ayarlar mantigini object ile modelleme

---
+++ js/odev32.js
@@ -0,0 +1,15 @@
+let appTheme = prompt(`Select your theme Dark/Light`)
+let appNotification = prompt(`Should notifications be enabled? True/False`)
+let appLanguage = prompt (`Write your language`)
+
+
+
+
+
+let appSettings = {
+  Theme: appTheme,
+  Notification: appNotification,
+  Language: appLanguage
+}
+
+console.log(appSettings)
