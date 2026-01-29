7de8cb2cbbe2ce21d00ff39691651ba484ee3807
Turgut <turcuteforwebsites@proton.me>
2026-01-29 14:17:13 +0300

    feat: odev43 tamamlandi - Kullanici bilgilerini key-value olarak listeleme

---
+++ js/odev43.js
@@ -0,0 +1,11 @@
+// Kullanıcı Bilgilerini Key-Value Olarak Listeleme
+
+let user = {
+  name: "Mehmet",
+  age: 32,
+  role: "admin"
+};
+
+let entries = Object.entries(user)
+
+console.log(entries)
