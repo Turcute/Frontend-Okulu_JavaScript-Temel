4df1fe252678023d4ce372c500917d12b94efcdf
Turgut <turcuteforwebsites@proton.me>
2026-02-19 10:01:12 +0300

    refactor: extract logic into function

--- js/odev50.js
+++ js/odev50.js
@@ -1,42 +1,45 @@
 //  Taş - Kağıt - Makas
 
-let userScore = 0;
-let computerScore = 0;
 
-while (true) {
-  let userSelect = prompt(`Choose one: Rock / Paper / Scissors (or type ‘exit’ to end the game)`)
+const startGame = () => {
+  let userScore = 0;
+  let computerScore = 0;
 
-  userSelect = userSelect.toLocaleLowerCase().trim();
+  while (true) {
+    let userSelect = prompt(`Choose one: Rock / Paper / Scissors (or type ‘exit’ to end the game)`)
 
-  if (userSelect === `exit`) {
-    console.log(`Game is over! Total score - You: ${userScore}, Computer: ${computerScore}`)
-    break;
-  }
+    userSelect = userSelect.toLocaleLowerCase().trim();
 
-  if (userSelect !== `rock` && userSelect !== `paper` && userSelect !== `scissors`) {
-    console.log(`Invalid selection! Please write ‘rock’, ‘paper’, or ‘scissors’.`)
-    continue;
-  }
+    if (userSelect === `exit`) {
+      console.log(`Game is over! Total score - You: ${userScore}, Computer: ${computerScore}`)
+      break;
+    }
 
-  let computerMove = [`rock`, `paper`, `scissors`].at(Math.floor(Math.random() * 3));
-  console.log(`Computer selection: ${computerMove}`);
+    if (userSelect !== `rock` && userSelect !== `paper` && userSelect !== `scissors`) {
+      console.log(`Invalid selection! Please write ‘rock’, ‘paper’, or ‘scissors’.`)
+      continue;
+    }
 
-  if (userSelect === computerMove) {
-    console.log(`Draw`)
-    console.log(`Score: You: ${userScore}, Computer: ${computerScore}`);
-  } else if (
-    (userSelect === `rock` && computerMove === `scissors`) ||
-    (userSelect === `paper` && computerMove === `rock`) ||
-    (userSelect === `scissors` && computerMove === `paper`)
-  ) {
-    console.log(`You win!`);
-    userScore++;
-    console.log(`Score - You: ${userScore}, Computer: ${computerScore}`);
-  } else {
-    console.log(`You lost!`);
-    computerScore++;
-    console.log(`Score - You: ${userScore}, Computer: ${computerScore}`)
+    let computerMove = [`rock`, `paper`, `scissors`].at(Math.floor(Math.random() * 3));
+    console.log(`Computer selection: ${computerMove}`);
+
+    if (userSelect === computerMove) {
+      console.log(`Draw`)
+      console.log(`Score: You: ${userScore}, Computer: ${computerScore}`);
+    } else if (
+      (userSelect === `rock` && computerMove === `scissors`) ||
+      (userSelect === `paper` && computerMove === `rock`) ||
+      (userSelect === `scissors` && computerMove === `paper`)
+    ) {
+      console.log(`You win!`);
+      userScore++;
+      console.log(`Score - You: ${userScore}, Computer: ${computerScore}`);
+    } else {
+      console.log(`You lost!`);
+      computerScore++;
+      console.log(`Score - You: ${userScore}, Computer: ${computerScore}`)
+    }
   }
 }
 
-
+startGame();
